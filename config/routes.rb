0aa34408cb3054342492002c5b1b63207537513e
Mike <vichnitchkine@gmail.com>
2024-02-05 18:51:57 -0500

    Added movie MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :movies
   get 'pages/home'
   get 'pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
